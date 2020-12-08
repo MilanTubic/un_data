8aa200a4539ba21ad59b50166437404cf758b380
Milan <72260820+MilanTubic@users.noreply.github.com>
2020-12-08 17:07:34 +0100

    Add files via upload

--- get_check_proxy.py
+++ get_check_proxy.py
@@ -5,24 +5,23 @@ import time
 
 
 def funkcija(prox):
-    urlpr = str("http://google.com")
-    start=time.perf_counter()
-    while round(time.perf_counter() - start) < 1 :
-      try :
-          r = requests.get(urlpr, proxies={"https" : prox, "http" : prox})
+    urlpr = str("http://comtrade.un.org")
+    start = time.perf_counter()
 
-          if r.status_code == 200 :
-              print(prox)
-              return prox
-              break
+    try:
+        r = requests.get(urlpr,timeout=14, proxies={"https": prox, "http": prox})
+        t1 = time.perf_counter() - start
+        if r.status_code == 200 and t1<14:
+            print(prox+" responded in "+str(t1)+" seconds")
+            r.connection.close()
+            return prox
 
-      except Exception as e :
-            print("next")
-            break
-
+    except Exception:
+        print("next")
 
 
-res =requests.get('https://free-proxy-list.net/', headers={'User-Agent' : 'Mozilla/5.0'})
+
+res = requests.get('https://free-proxy-list.net/',headers={'User-Agent':'Mozilla/5.0'})
 soup = BeautifulSoup(res.text, "lxml")
 
 proxy_l = []
@@ -34,16 +33,23 @@ print(proxy_l)
 
 proxy_ok=[]
 # try :
+
 with concurrent.futures.ThreadPoolExecutor() as executor:
     futures = []
-    for item in proxy_l:
-         futures.append(executor.submit(funkcija,prox=item))
+    start = time.perf_counter()
+    futures = [executor.submit(funkcija, p) for p in proxy_l]
     try :
+        for future in concurrent.futures.as_completed(futures, timeout=300):
 
-        for future in concurrent.futures.as_completed(futures) :
-            if future.result()!= None:
+            if future.result() != None:
                 proxy_ok.append(future.result())
-    except Exception as e :
-        print(e)
+
+    except concurrent.futures.TimeoutError:
+        print("This took to long...")
+
+
+    finally :
+        executor.shutdown()
+
 
 print(proxy_ok)
